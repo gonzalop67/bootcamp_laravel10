e54b40f5c95d5019494adcc38f32c18033cd867f
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2024-10-11 20:32:15 -0500

    Utilizando controladores en Laravel 10

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -9,7 +9,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form action="" method="POST">
+                    <form action="{{ route('chirps.store') }}" method="POST">
                         @csrf
                         <textarea name="message"
                             class="block w-full transition-colors duration-300 bg-white border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Chirp;
 use Illuminate\Support\Facades\Route;
@@ -14,20 +15,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/chirps', function () {
-        return view('chirps.index');
-    })->name('chirps.index');
-
-    Route::post('/chirps', function () {
-        // Insert into database
-        Chirp::create([
-            'message' => request('message'),
-            'user_id' => auth()->id(),
-        ]);
-
-        return to_route('chirps.index')
-            ->with('status', __('Chirp created successfully'));
-    });
+    Route::get('/chirps', [ChirpController::class, 'index'])->name('chirps.index');
+    Route::post('/chirps', [ChirpController::class, 'store'])->name('chirps.store');
 });
 
 require __DIR__ . '/auth.php';

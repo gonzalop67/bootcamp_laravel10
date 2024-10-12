9c52da09dca7fbc6c2dd040f9e4e3bede9117fe2
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2024-10-12 07:01:44 -0500

    Actualizar registros en base de datos

--- resources/views/chirps/edit.blade.php
+++ resources/views/chirps/edit.blade.php
@@ -9,8 +9,9 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form action="#" method="POST">
+                    <form action="{{ route('chirps.update', $chirp) }}" method="POST">
                         @csrf
+                        @method('PUT')
                         <textarea name="message"
                             class="block w-full transition-colors duration-300 bg-white border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
                             placeholder="{{ __('What\'s on your mind?') }}">{{ old('message', $chirp->message) }}</textarea>
